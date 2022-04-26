2fdc0b553e61e5006411a661b157b04b712d2881
umakant-2113 <lodhiumakant800@gmail.com>
2022-04-26 11:42:48 +0530

    done

--- block-BNaaaj/index.js
+++ block-BNaaaj/index.js
@@ -1,9 +1,5 @@
-let buff1=Buffer.alloc(12);
-let value=buff1.write('hello umakant rajput');
-console.log(buff1.toString())
-
-
 console.log('Welcome to Nodejs')
+
 
 let os=require('os');
 var free=os.freemem();
@@ -11,6 +7,16 @@ var cpus=os.cpus().length
 var version=os.version()
 var uptime=os.uptime()
 console.log(free,cpus,version,uptime)
+var {readFile,unlink}=require('fs')
+
+
+let buff1=Buffer.alloc(12);
+let value=buff1.write('hello umakant rajput');
+console.log(buff1.toString())
+
+
+
+
 
 let url=`https://airindia.com/fares/calculate?from=delhi&to=detroit`;
 let path=require('url');
